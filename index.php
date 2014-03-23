e5f6b6c839872c75f02964c57e11d18d8c371e41
ws23 <linyutsu23@gmail.com>
2014-03-23 16:15:47 +0800

    update preparing page

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php require_once('header.php');?>
 <body>
 <p align="center"><img src="img/preparing.gif" alt="網頁準備中！"></img></p>
+<p align="center" style="font-size:20pt; color:#777777">網頁準備中！歡迎至<a href="http://fb.com/NDHUCSIECamp14" target="_blank">粉絲專頁</a>看看喔！</p>
 </body>
 <?php require_once('bottom.php');?>
